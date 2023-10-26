517fa10f0db9cfd082ed3b9723ea53515abe05f5
Megha <134916418+m21gha@users.noreply.github.com>
2023-10-26 16:19:53 +1100

    refined the 3D reconstruction

--- Project5.m
+++ Project5.m
@@ -5,7 +5,9 @@ classdef Project5 %This is the main class for project 5: 3D reconsutrction using
  methods
       function self = Project5()
           self.calibration()
-          self.coordinatetransformation()                            
+          self.coordinatetransformation()
+          self.displayComparisonImages()
+          self.fusedPointCloud()
         end
   end
 
@@ -47,8 +49,7 @@ classdef Project5 %This is the main class for project 5: 3D reconsutrction using
                     synchronizedData{i, 2} = timestampPose;       % Corresponding EM sensor pose
     
                 end
-            
-            
+ 
             calibrationResults = load('Calib_Results.mat');  % Load calibration results
 
             % % Initialize an empty cell array to store calibrated data
@@ -85,7 +86,6 @@ classdef Project5 %This is the main class for project 5: 3D reconsutrction using
                     H_calibration(1:3, 1:3) = R_calib;
                     H_calibration(1:3, 4) = calibrationResults.(['Tc_' num2str(i)]); % Access Tc for the i-th image
     
-    
                     H_calibrated = H_calibration * H_timestamp;
                     
                     % Extract calibrated translation and rotation
@@ -95,8 +95,8 @@ classdef Project5 %This is the main class for project 5: 3D reconsutrction using
                 end
 
                 %Calculate the average calibrated translations and rotations
-                avgCalibratedTranslation = mean(cell2mat(calibratedTranslations), 1)
-                avgCalibratedRotation = mean(cell2mat(calibratedRotations), 1)
+                %avgCalibratedTranslation = mean(cell2mat(calibratedTranslations), 1)
+                %avgCalibratedRotation = mean(cell2mat(calibratedRotations), 1)
                               
         end
 
@@ -122,7 +122,6 @@ classdef Project5 %This is the main class for project 5: 3D reconsutrction using
             % Create a 3D point cloud
             point_cloud = cat(3, X_3D, Y_3D, Z);
            
-            
             % Display the point cloud using pcshow
             figure;
             pcshow(point_cloud);
@@ -135,11 +134,161 @@ classdef Project5 %This is the main class for project 5: 3D reconsutrction using
             
             view(0, -90); % Adjust the view as needed
 
+        end
+
+        function displayComparisonImages()
+            % Load the original depth image
+            original_depth_data = readNPY('depthimagesdepth_image_0.npy');  % Replace with the path to your original depth image
+            
+            % Load the filtered depth image
+            filtered_depth_data = readNPY('filtered_depth1.npy');  % Replace with the path to your filtered depth image
+            
+            % Load the outlier-removed depth image
+            outlier_removed_data = readNPY('filtered_depth1_outlier_removed.npy');  % Replace with the path to your outlier-removed depth image
+            
+            % Display the images side by side for comparison
+            figure;
+            
+            % Display the original depth image
+            subplot(1, 3, 1);
+            imshow(original_depth_data, []);
+            title('Original Depth Image');
+            
+            % Display the filtered depth image
+            subplot(1, 3, 2);
+            imshow(filtered_depth_data, []);
+            title('Filtered Depth Image');
+            
+            % Display the outlier-removed depth image
+            subplot(1, 3, 3);
+            imshow(outlier_removed_data, []);
+            title('Outlier-Removed Depth Image');
+        end
+
+
+         function fusedPointCloud(~)
+    
+            % Define the directory where your filtered depth data is stored
+            dataDir = 'F:\UTS\2023 subjects\2023 Semester 2 Spring\Sensors and Control for Mechatronic Systems\Group Project\Github\Project5\Datas\outlierRemovedDepthData';  % Replace with the actual directory
+        
+            % List all .npy files in the directory
+            filteredDepthFiles = dir(fullfile(dataDir, '*.npy'));
+            
+            %Define the directory where your depth data files are stored
+            depthdataDir = 'F:\UTS\2023 subjects\2023 Semester 2 Spring\Sensors and Control for Mechatronic Systems\Group Project\Github\Project5\Datas\depthdata'; % Replace with the actual directory  
+            % List all .npy files in the directory
+            depthDataFiles = dir(fullfile(depthdataDir, 'depthimagesdepth_image_*.npy'));
+           
+             % Camera intrinsic parameters from the calibrationResults
+            fx = 696.631370276213829;
+            fy = 688.430910512066930;
+            cx = 343.7744401879735965;
+            cy = 304.051147293626400;
+        
+            % Create an empty point cloud to store all data
+            fusedPointCloud = pointCloud(zeros(0, 3, 'single'));
+        
+            for i = 1:numel(depthDataFiles)
+                % Load the depth data
+                depth_data = readNPY(fullfile(depthdataDir, depthDataFiles(i).name));
+        
+                % Convert depth image to 3D point cloud
+                [height, width] = size(depth_data);
+                [Y, X] = ndgrid(1:height, 1:width);
+                Z = double(depth_data) / 1000.0;  % Convert depth to meters
+                X_3D = (X - cx) .* Z / fx;
+                Y_3D = (Y - cy) .* Z / fy;
+        
+                % Create a 3D point cloud from the current data
+                currentPointCloud = pointCloud(cat(3, X_3D, Y_3D, Z), 'Color', uint8(ones(height, width, 3)));
+        
+                % Merge the current point cloud with the fused point cloud
+                fusedPointCloud = pcmerge(fusedPointCloud, currentPointCloud, 0.001); % Adjust the merge threshold
             end
+        
+            % Display the fused point cloud
+            figure;
+            pcshow(fusedPointCloud);
+            title('Fused 3D Point Cloud');
+        
+    
+        end   
+
+        
+
+%%
+        function noiseReductionAndSace()
+
+            % Specify the folder where your depth data files are located
+            data_folder = 'F:\UTS\2023 subjects\2023 Semester 2 Spring\Sensors and Control for Mechatronic Systems\Group Project\Github\Project5\Datas\depthdata';
+        
+            % Get a list of all depth data files in the folder
+            depth_data_files = dir(fullfile(data_folder, 'depthimagesdepth_image_*.npy'));
+            
+            %folder to save processed depth images
+            output_folder = 'F:\UTS\2023 subjects\2023 Semester 2 Spring\Sensors and Control for Mechatronic Systems\Group Project\Github\Project5\Datas\filteredDepthData'; % Adjust the path
+            if ~exist(output_folder, 'dir')
+                mkdir(output_folder);
+            end
+        
+            % Loop through each depth data file
+            for i = 1:numel(depth_data_files)
+                % Load depth data
+                depth_data = readNPY(fullfile(depth_data_files(i).folder, depth_data_files(i).name));
+        
+                % Apply noise reduction ( Gaussian smoothing)
+                % Apply Gaussian smoothing
+                sigma = 2.0;  % Adjust the standard deviation as needed
+                filtered_depth_data = imgaussfilt(depth_data, sigma); % Implement your noise reduction function
+        
+                % Save the processed depth image to the output folder
+                output_filename = fullfile(output_folder, sprintf('filtered_depth%d.npy', i));
+                writeNPY(filtered_depth_data, output_filename);
+            end
+               
+
+        end
+
+%%
+        function outlierRemovalAndSave()
+        
+            % Define the folder where filtered depth files are located
+            filtered_folder = 'F:\UTS\2023 subjects\2023 Semester 2 Spring\Sensors and Control for Mechatronic Systems\Group Project\Github\Project5\Datas\filteredDepthData'; % Adjust the path
+            
+            % Create a pattern to match specific file names (e.g., 'filtered_depth*.npy')
+            file_pattern = 'filtered_depth*.npy';
+            
+            % List files in the folder that match the pattern
+            filtered_depth_files = dir(fullfile(filtered_folder, file_pattern));
+
+            %folder to save the outlier-removed depth images
+            output_folder = 'F:\UTS\2023 subjects\2023 Semester 2 Spring\Sensors and Control for Mechatronic Systems\Group Project\Github\Project5\Datas\outlierRemovedDepthData'; % Adjust the path
+            if ~exist(output_folder, 'dir')
+                mkdir(output_folder);
+            end
+        
+            % Define the neighborhood size for the median filter
+            neighborhood_size = [3, 3]; % Adjust the size as needed
+        
+            % Loop through each filtered depth data file
+            for i = 1:numel(filtered_depth_files)
+                % Load filtered depth data
+                filtered_depth_data = readNPY(fullfile(filtered_depth_files(i).folder, filtered_depth_files(i).name));
+        
+                % Apply median filtering for outlier removal
+                outlier_removed_data = medfilt2(filtered_depth_data, neighborhood_size);
+        
+                % Save the outlier-removed depth image to the output folder
+                [~, name, ~] = fileparts(filtered_depth_files(i).name);  % Extract file name without extension
+                output_filename = fullfile(output_folder, [name, '_outlier_removed.npy']);
+                writeNPY(outlier_removed_data, output_filename);
+            end
+        
+        end
 
 
-
-    function calibrationToolbox()
+%%
+    function calibrationToolboxAndSave()
             
             %this is conductng calibration algorithim using the 
             %toolbox_calib provided by UTS
@@ -200,7 +349,6 @@ classdef Project5 %This is the main class for project 5: 3D reconsutrction using
             end
             
             % Instructions for using toolbox_calib
-  
                 %to Calibrate the saved images
                 %calib_gui
                 %select "Standard"
